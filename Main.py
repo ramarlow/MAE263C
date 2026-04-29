951818f3669ca4afa73f4fce4c33a6565fcb364b
ramarlow <98787850+ramarlow@users.noreply.github.com>
2026-04-29 12:32:29 -0700

    added hardware interfacing and data publishing to Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,84 @@
+from dynamixel_sdk import *
 from forward_kinematics import forward_kinematics
+from kinematics import Jacobian
 from operation_space_PID import PDController
 import numpy as np
+import socket
+from json import dumps
 
-theta1 = np.pi/2
-theta2 = np.pi/2
+class UDP_Client:
+    def __init__(self, host: str = "127.0.0.1", port: int = 9870):
+        self._UDP_addr = (host, port)
+        self._UDP_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    def send(self, data: dict):
+        '''send data to UDP server, used for plotting with plotjuggler'''
+        try:
+            data["time"] = round(time.time(), 3)
+            # print(data)
+            self._UDP_sock.sendto(bytes(dumps(data),encoding='utf-8'), self._UDP_addr)
+        except Exception as e:
+            print(f'Send failed: {e}')
+
+    def close(self):
+        self._UDP_sock.close()
+
+    def __del__(self):
+        self.close()
+
+data_out = UDP_Client()
+PORT = "COM10"
+BAUD = 57600
+IDS = [5, 6]
+
+def ticks_to_deg(ticks):
+    return ticks * 2 * np.pi / 4096.0
+
+port = PortHandler(PORT)
+packet = PacketHandler(2.0)
+
+port.openPort()
+port.setBaudRate(BAUD)
+
+controller = PDController(setpoint=[.1, 0.350], kp=50.0, kd=50.0, dt=0.01)
+
+for id in IDS:
+    packet.write1ByteTxRx(port, id, 11, 16)  # PWM mode
+    packet.write1ByteTxRx(port, id, 64, 1)   # torque enable
+
+def ticks_to_deg(ticks):
+    return ticks * 2 * np.pi / 4096.0
 
 D = .200
 L1 = .220
 L2 = .250
 
-pos = forward_kinematics(theta1, theta2, L1, L2, D)
-controller = PDController(setpoint=[.1, 0.450], kp=50.0, kd=10.0, dt=0.01)
+while 1:
+    for id in IDS:
+        pos, _, _ = packet.read4ByteTxRx(port, id, 132)
+        # print(pos)
+        if pos > 0x7FFFFFFF:
+            pos -= 0x100000000
 
-# Each timestep, call:
-force = controller.update(x=pos[0], y=pos[1])
-# returns np.array([Fx, Fy])
-print(force)
+        angle = ticks_to_deg(pos)
+        if id == 6:
+            theta1 = np.pi-angle
+        else:
+            theta2 = 2*np.pi-angle
+
+    pos = forward_kinematics(theta1, theta2, L1, L2, D)
+
+    # Each timestep, call:
+    force = controller.update(x=pos[0], y=pos[1])
+    # returns np.array([Fx, Fy])
+    
+
+    tau = Jacobian(theta1,theta2).T@force.T
+
+    K_PWM = 0.04
+
+    packet.write2ByteTxRx(port, 6, 100, int(tau[0]*K_PWM) & 0xFFFF)
+    packet.write2ByteTxRx(port, 5, 100, int(tau[1]*K_PWM) & 0xFFFF)
+
+    print(f'force:{force}, torques:{tau}')
 
