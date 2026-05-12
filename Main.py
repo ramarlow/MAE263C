203ce993e0f646eb4787d1fdee743e3fef7ab689
ramarlow <98787850+ramarlow@users.noreply.github.com>
2026-05-12 13:01:13 -0700

    impedance works sorta okay but not isotropic

--- Main.py
+++ Main.py
@@ -40,7 +40,10 @@ packet = PacketHandler(2.0)
 port.openPort()
 port.setBaudRate(BAUD)
 
-controller = PDController(setpoint=[.1, 0.350], kp=50.0, kd=50.0, dt=0.01)
+controller = PDController(setpoint=[.1, 0.350], 
+                          kp=50.0, 
+                          kd=0,#50.0, 
+                          dt=0.01)
 
 for id in IDS:
     packet.write1ByteTxRx(port, id, 11, 16)  # PWM mode
@@ -53,32 +56,42 @@ D = .200
 L1 = .220
 L2 = .250
 
-while 1:
+try:
+    while 1:
+        for id in IDS:
+            pos, _, _ = packet.read4ByteTxRx(port, id, 132)
+            # print(pos)
+            if pos > 0x7FFFFFFF:
+                pos -= 0x100000000
+
+            angle = ticks_to_deg(pos)
+            if id == 6:
+                theta2 = np.pi-angle
+            else:
+                theta1 = 2*np.pi-angle
+
+        pos = forward_kinematics(theta1, theta2, L1, L2, D)
+
+        # Each timestep, call:
+        force = controller.update(x=pos[0], y=pos[1])
+        # returns np.array([Fx, Fy])
+        
+
+        tau = Jacobian(theta1,theta2).T@force.T
+
+        K_PWM = -0.8
+
+        packet.write2ByteTxRx(port, 5, 100, int(tau[0]*K_PWM) & 0xFFFF)
+        packet.write2ByteTxRx(port, 6, 100, int(tau[1]*K_PWM) & 0xFFFF)
+
+        print(f'force:{force}, torques:{tau}, PWMS:{tau*K_PWM}')
+        data_out.send({
+                'q1':float(theta1),'q2':float(theta2),
+                'ex':float(pos[0]),'ey':float(pos[1]),
+                'fx':float(force[0]),'fy':float(force[1]),
+                'tau1':float(tau[0]),'tau2':float(tau[1])})
+finally:
     for id in IDS:
-        pos, _, _ = packet.read4ByteTxRx(port, id, 132)
-        # print(pos)
-        if pos > 0x7FFFFFFF:
-            pos -= 0x100000000
-
-        angle = ticks_to_deg(pos)
-        if id == 6:
-            theta1 = np.pi-angle
-        else:
-            theta2 = 2*np.pi-angle
-
-    pos = forward_kinematics(theta1, theta2, L1, L2, D)
-
-    # Each timestep, call:
-    force = controller.update(x=pos[0], y=pos[1])
-    # returns np.array([Fx, Fy])
-    
-
-    tau = Jacobian(theta1,theta2).T@force.T
-
-    K_PWM = 0.04
-
-    packet.write2ByteTxRx(port, 6, 100, int(tau[0]*K_PWM) & 0xFFFF)
-    packet.write2ByteTxRx(port, 5, 100, int(tau[1]*K_PWM) & 0xFFFF)
-
-    print(f'force:{force}, torques:{tau}')
-
+        packet.write2ByteTxRx(port, id, 100, 0)
+        packet.write1ByteTxRx(port, id, 64, 0)
+    port.closePort()
