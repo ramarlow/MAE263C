8d8cedb32359a162a4fbecf461f674b2d3b6fed0
ramarlow <98787850+ramarlow@users.noreply.github.com>
2026-05-17 17:56:36 -0700

    Change impedance controller to calculate timestep instead of assuming

--- operation_space_PID.py
+++ operation_space_PID.py
@@ -1,4 +1,5 @@
 import numpy as np
+from time import time
 
 class PDController:
     def __init__(self, setpoint, kp=50.0, kd=10.0, dt=0.01):
@@ -6,6 +7,7 @@ class PDController:
         self.kp = kp
         self.kd = kd
         self.dt = dt
+        self.prev_t = time()
         self.velocity = np.zeros(2)
         self.prev_pos = None
 
@@ -15,6 +17,9 @@ class PDController:
         Returns force vector [Fx, Fy] in Newtons.
         """
         current_pos = np.array([x, y], dtype=float)
+        current_t = time()
+        self.dt = current_t - self.prev_t
+        self.prev_t = current_t
 
         # Estimate velocity from position difference if not provided externally
         if self.prev_pos is not None:
