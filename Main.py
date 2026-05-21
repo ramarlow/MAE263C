fixed jacobian, added forward dynamics

Main.py
@@ -40,8 +40,8 @@ except:
     arduino = None
 
 controller = PDController(setpoint=[.1, 0.350], 
-                          kp=50.0, 
-                          kd=0.,#50.0, 
+                          kp=20.0, 
+                          kd=0.5,#50.0, 
                           dt=0.01)
 
 for id in IDS:
@@ -78,6 +78,8 @@ try:
         
         tau = Jacobian(theta1,theta2).T@force.T
 
+        f_cells = -dynamics.F_ee(loads, np.array([theta1,theta2]))
+
         K_PWM = 0.8
 
         packet.write2ByteTxRx(port, 5, 100, int(tau[0]*-K_PWM) & 0xFFFF)
@@ -90,6 +92,7 @@ try:
                 'fx':float(-force[0]),'fy':float(force[1]), # desired ee force
                 'tau1':float(tau[0]),'tau2':float(tau[1]),  # desired joint torques
                 'f1':float(loads[0]),'f2':float(loads[1]),  # load cell readings
+                'fx_cell':float(f_cells[0]),'fy_cell':float(f_cells[1]), # transformed load cell readings
                 })
 finally:
     for id in IDS:

dynamics.py
@@ -5,8 +5,18 @@ forward dynamics math for finding end effector forces given load cell shear forc
 import numpy as np
 from kinematics import FK
 
-def F_ee(f1, f2=None):
+def F_ee(f, q):
     '''
     manipulator end effector forces based on load cell readings
     '''
-    return 
+    f1 = f[0]; f2 = f[1];
+    q1 = q[0]; q2 = q[1];
+
+    Pe, P2, P4, Ph = FK(q1,q2)
+
+    u_12 = np.array([np.cos(q1),np.sin(q1)])
+    t_2 = np.array([-u_12[0],u_12[1]])
+    u_54 = np.array([np.cos(q2),np.sin(q2)])
+    t_4 = np.array([u_54[0],-u_54[1]])
+
+    return f1*t_2 + f2*t_4

kinematics.py
@@ -129,15 +129,15 @@ def Jacobian(q, q2=None):
     d1dxh = d1dx2 + (d1b*d - d1d*b)/d**2 * x4_x2 + b/d*(d1dx4 - d1dx2) # (20)
     d5dxh = d5dx2 + (d5b*d - d5d*b)/d**2 * x4_x2 + b/d*(d5dx4 - d5dx2)
 
-    d1dy3 = d1dyh - h/d*(d1dx4 - d1dx2) - (d1h*d - d1d*h)/d**2 * x4_x2 # (21)
-    d5dy3 = d5dyh - h/d*(d5dx4 - d5dx2) - (d5h*d - d5d*h)/d**2 * x4_x2
+    d1dy3 = d1dyh + h/d*(d1dx4 - d1dx2) + (d1h*d - d1d*h)/d**2 * x4_x2 # (21)
+    d5dy3 = d5dyh + h/d*(d5dx4 - d5dx2) + (d5h*d - d5d*h)/d**2 * x4_x2
 
-    d1dx3 = d1dxh + h/d*(d1dy4 - d1dy2) + (d1h*d - d1d*h)/d**2 * y4_y2 # (22)
-    d5dx3 = d5dxh + h/d*(d5dy4 - d5dy2) + (d5h*d - d5d*h)/d**2 * y4_y2
+    d1dx3 = d1dxh - h/d*(d1dy4 - d1dy2) - (d1h*d - d1d*h)/d**2 * y4_y2 # (22)
+    d5dx3 = d5dxh - h/d*(d5dy4 - d5dy2) - (d5h*d - d5d*h)/d**2 * y4_y2
 
     return np.array([                                           # (13)
-        [d5dx3, d1dx3],
-        [d5dy3, d1dy3]])
+        [d1dx3, d5dx3],
+        [d1dy3, d5dy3]])
 
 
 def IK_numerical(Pe_des,q_guess=np.array([np.pi/2,np.pi/2]).T,tol=1e-3,maxiters=100):